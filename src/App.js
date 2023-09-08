2944b1189b5a58365be12e006b588085e53708fb
Felipe Torlai <felipe.c.torlai@gmail.com>
2023-09-08 18:53:39 -0300

    Mudanças feitas

--- src/App.js
+++ src/App.js
@@ -5,16 +5,25 @@ function App() {
   return (
     <div>
       <header>
-        <h1>Minha Página Inicial</h1>
+
+        {/* Aba de pesquisa */}
+        <div className="search">
+          <input type="text" placeholder="Pesquisar..." />
+          <button>Login</button>
+        </div>
       </header>
 
-      <section className="search">
-        <input type="text" placeholder="Pesquisar..." />
-        <button>Pesquisar</button>
-      </section>
+      {/* Botões "Sobre nós", "Ajuda" e "Blog" */}
+      <div className="buttons">
+        <button>Sobre nós</button>
+        <button>Ajuda</button>
+        <button>Blog</button>
+      </div>
+
+      <h2>Conheça Nossos Serviços</h2>
 
       <section className="services">
-        <h2>Conheça Nossos Serviços</h2>
+        <br></br>
         <div className="service">
           <h3>Seguro Moto</h3>
           <p>Oferecemos seguros de moto para garantir sua segurança nas estradas.</p>
@@ -25,6 +34,10 @@ function App() {
         </div>
         <div className="service">
           <h3>Serviço de Guincho</h3>
+          <p>Estamos prontos para ajudar em situações de emergência com nosso serviço de guincho.</p>
+        </div>
+        <div className="service">
+          <h3>Fale Conosco</h3>
           <p>Estamos prontos para ajudar em situações de emergência com nosso serviço de guincho.</p>
         </div>
       </section>
