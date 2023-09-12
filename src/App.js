fec9c78973ad8ba9b788cb987818d4414531e82d
Felipe Torlai <felipe.c.torlai@gmail.com>
2023-09-11 22:26:18 -0300

    Mudanças

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ function App() {
       {/* Botões "Sobre nós", "Ajuda" e "Blog" */}
       <div className="buttons">
         <button>Sobre nós</button>
-        <button>Ajuda</button>
-        <button>Blog</button>
+        <button>Chat</button>
+        <button>Contato</button>
       </div>
 
       <h2 className='subtitulos'>Conheça Nossos Serviços</h2>
@@ -43,8 +43,12 @@ function App() {
       <div className='texto-tips'>
         <p>Adquira o chip internacional e não perca mais tempo com planos de internet fora do país! Venha fazer parte você também!</p>
       </div>
-
-
+      <div className='texto-tips'>
+        <p>Malas prontas para se aventurar na Ásia? Antes vale a pena contratar o chip internacional para a Malásia e viver essa experiência 100% conectado!</p>
+      </div>
+      <div className='texto-tips'>
+        <p>O seguro viagem para Bermudas é obrigatório. Descubra agora quais são as melhores opções, aproveite para contratar o seu.</p>
+      </div>
 
       <footer className='footer'>
         <p>“A Porto Seguro projeta segurança pensando em você, e lidera o setor de seguros com suas inovações.”</p>
---
+++ src/rm.md
@@ -0,0 +1,7 @@
+#Turma TDSPZ
+
+Felipe Cardoso Torlai - RM550263
+Matheus Estevo - RM550913
+Jean Carlos - RM550430
+Alef Gabriel - RM99487
+Cayque Pereira - RM551353
